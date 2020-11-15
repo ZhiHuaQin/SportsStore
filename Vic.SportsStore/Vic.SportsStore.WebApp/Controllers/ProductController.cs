a4133bd5d4b43c1d13108e559e3665a932b0cfd8
DESKTOP-D85I4D5\ZhiHua <yunhual@yahoo.com>
2020-11-15 18:33:44 -0500

    1.[NEW] Paging Links

--- Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Vic.SportsStore.Domain.Abstract;
 using Vic.SportsStore.Domain.Concrete;
+using Vic.SportsStore.WebApp.Models;
 
 namespace Vic.SportsStore.WebApp.Controllers
 {
@@ -17,13 +18,29 @@ namespace Vic.SportsStore.WebApp.Controllers
 
         public ViewResult List(int page = 1)
         {
-            var model = ProductsRepository
-                .Products
-                .OrderBy(p => p.ProductId)
-                .Skip((page - 1) * PageSize)
-                .Take(PageSize)
-                ;
+            ProductsListViewModel model = new ProductsListViewModel
+            {
+                Products = ProductsRepository
+                    .Products
+                    .OrderBy(p => p.ProductId)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = ProductsRepository.Products.Count()
+                }
+            };
             return View(model);
+            //var model = ProductsRepository
+            //    .Products
+            //    .OrderBy(p => p.ProductId)
+            //    .Skip((page - 1) * PageSize)
+            //    .Take(PageSize)
+            //    ;
+            //return View(model);
             // return View(ProductsRepository.Products);    
         }
     }
