f09147f2cfe23a48c22f7a22dd0c65792d1db753
DESKTOP-D85I4D5\ZhiHua <yunhual@yahoo.com>
2020-11-22 16:18:22 -0500

    1.[NEW] add category menu

---
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Controllers/NavController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vic.SportsStore.Domain.Abstract;
+using Vic.SportsStore.Domain.Concrete;
+
+namespace Vic.SportsStore.WebApp.Controllers
+{
+    public class NavController : Controller
+    {
+        //private IProductsRepository repository;
+        //public NavController(IProductsRepository repo)
+        //{
+        //    repository = repo;
+        //}
+
+        public IProductsRepository ProductsRepository { get; set; }
+            = new EFProductRepository();
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = ProductsRepository
+            .Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x);
+
+            return PartialView(categories);
+        }
+    }
+}
