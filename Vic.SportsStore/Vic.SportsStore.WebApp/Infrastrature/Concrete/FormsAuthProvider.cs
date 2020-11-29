3308a1311eed74229084e69111beb07186146566
DESKTOP-D85I4D5\ZhiHua <yunhual@yahoo.com>
2020-11-29 17:45:39 -0500

    1.[NEW] login through DB admin

--- Vic.SportsStore/Vic.SportsStore.WebApp/Infrastrature/Concrete/FormsAuthProvider.cs
+++ Vic.SportsStore/Vic.SportsStore.WebApp/Infrastrature/Concrete/FormsAuthProvider.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
+using Vic.SportsStore.Domain.Concrete;
 using Vic.SportsStore.WebApp.Infrastrature.Abstract;
 
 namespace Vic.SportsStore.WebApp.Infrastrature.Concrete
 {
     public class FormsAuthProvider : IAuthProvider
     {
+        private EFDbContext context = new EFDbContext();
         public bool Authenticate(string username, string password)
         {
+            bool result = false;
+            var user = context.AdminUsers.FirstOrDefault(x => x.UserName == username);
 
-#pragma warning disable CS0618 // Type or member is obsolete
-            bool result = FormsAuthentication.Authenticate(username, password);
-#pragma warning restore CS0618 // Type or member is obsolete
-
+            if(user!=null && user.Password == password)
+            {
+                result = true;
+            }
             if (result)
             {
                 FormsAuthentication.SetAuthCookie(username, false);
